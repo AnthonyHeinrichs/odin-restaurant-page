cb4d68d3047b917380d1421f5d5ba723de235ecc
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-11 13:03:43 +0200

    Listening to each navbar button and rendering pages (only console logs atm) dynamically per click

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import Home from './pages/home'
 import Contact from './pages/contact'
 import Menu from './pages/menu'
 
-Navbar()
-Home()
-Contact()
-Menu()
+function initialPageLoad() {
+  Navbar()
+  const contentDiv = document.getElementById('content')
+  const contentAfterNav = document.createElement('div')
+  contentAfterNav.id = 'contentAfterNav'
+  contentDiv.appendChild(contentAfterNav)
+  
+  const homeButton = document.getElementById('Home')
+  const menuButton = document.getElementById('Menu')
+  const contactButton = document.getElementById('Contact')
+  
+  homeButton.addEventListener('click', () => {
+    Home()
+  })
+  
+  menuButton.addEventListener('click', () => {
+    Menu()
+  })
+  
+  contactButton.addEventListener('click', () => {
+    Contact()
+  })
+}
+
+initialPageLoad()
