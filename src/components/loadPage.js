b77a26cb7600a8bd48328d1f17a51fe8ad37a273
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-12 12:01:09 +0200

    Moved initial page load function to seperate module per requirements in TOD

---
+++ src/components/loadPage.js
@@ -0,0 +1,38 @@
+import Navbar from './navbar'
+import Home from '../pages/home'
+import Contact from '../pages/contact'
+import Menu from '../pages/menu'
+
+export default function initialPageLoad() {
+  Navbar()
+
+  // creating a div for all content after the navbar
+  const contentDiv = document.getElementById('content')
+  const contentAfterNav = document.createElement('div')
+  contentAfterNav.id = 'contentAfterNav'
+  contentDiv.appendChild(contentAfterNav)
+
+  // initially loading the home page
+  Home()
+  
+  // grabbing all buttons for listening
+  const homeButton = document.getElementById('Home')
+  const menuButton = document.getElementById('Menu')
+  const contactButton = document.getElementById('Contact')
+  
+  // navbar buttons that listen for a click and render the page dynamically
+  homeButton.addEventListener('click', () => {
+    contentAfterNav.replaceChildren()
+    Home()
+  })
+  
+  menuButton.addEventListener('click', () => {
+    contentAfterNav.replaceChildren()
+    Menu()
+  })
+  
+  contactButton.addEventListener('click', () => {
+    contentAfterNav.replaceChildren()
+    Contact()
+  })
+}
